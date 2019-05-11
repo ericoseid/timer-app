a64beb15fa9c321243de5ee89401403fd6b1b9b6
Eric Oseid <ericoseid@gmail.com>
2019-05-11 12:33:48 -0700

    Creating a box component for rendering box data

--- src/box-component.js
+++ src/box-component.js
@@ -3,10 +3,29 @@
 import React from 'react';
 
 export default class BoxComponent extends React.Component {
+	constructor(props) {
+		super(props);	
+
+		this.createProperties = this.createProperties.bind(this);
+	}
+
+	createProperties() {
+		let upperLeft = this.props.data.upperLeft;
+		console.log(upperLeft);
+		return (
+			{className : 'box',
+			style : {top : upperLeft.y,
+							left : upperLeft.x
+							}
+			}
+		);	
+	}
 
 	render() {
 		return (
-			React.createElement('div', null, 'Fuck webpack')
+			React.createElement('div', 
+												  this.createProperties(), 
+													null)
 		);	
 	}
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import BoxComponent from './box-component';
+import Point from './point';
+import Box from './box';
 
 const div = document.createElement('div');
 div.id = 'root';
 document.body.append(div);
 
+let p = new Point(10, 10);
+let b = new Box(p, 1);
+
 ReactDOM.render(
-	React.createElement(BoxComponent, null, null),
+	React.createElement(BoxComponent, {data : b}, null),
 	document.getElementById('root')
 );
 
